d422935ff634a05be3d8575980a643d06ea680c4
Katerinka <e.kushner@bk.ru>
2023-08-07 18:37:51 +0300

    feat: homework for 8th lesson

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,109 +1,40 @@
 // 1 задание
-// Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
+// Необходимо с помощью цикла for вывести следующие 11 строк в консоль:
+// 0 – это ноль
+// 1 – нечетное число
+// 2 – четное число
+// 3 – нечетное число
+// …
+// 10 – четное число
 
-// Я не поняла как правильно выполнить задание, непонятно написано. То ли возводить число пользователя в куб, то ли сложить два числа, возведенных в куб. Поэтому у меня решение, скорее всего, корявее некуда
-
-let userNumbers = prompt("Введите два числа через запятую или пробел", "");
-let arr = Array.from(userNumbers)
-
-let newArr = [arr.shift(), arr.pop()]
-console.log(newArr);
-
-const countNumbers = (arr) => {
-  let result = 0;
-
-  for(let i = 0; i < arr.length; i++) {
-    result += arr[i] ** 3;
-  }
-
-  return result;
+for(let i = 0; i < 11; i++) {
+  console.log(i);
 }
-
-console.log("1 задание");
-console.log(countNumbers(newArr));
-
 
 // 2 задание
-// Пользователь вводит с клавиатуры число, если ввёл текст, необходимо вывести что значение задано неверно
-// Создать фукнцию, которая высчитывает 13% от данного числа и выводит в консоль текст "Размер заработной платы за вычетом налогов равен значение"
+// Дан массив [1, 2, 3, 4, 5, 6, 7]
+// Сделайте из этого массива следующий [1, 2, 3, 6, 7]
 
-const func = (answer) => {
-  if(isNaN(answer)) {
-    alert("Значение задано неверно");
-    return func(prompt("Сколько Вы получаете в месяц?"))
-  } else return answer
-}
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let part1 = arr.slice(0, 3);
+let part2 = arr.slice(5, 7);
 
-const responce = func(+prompt("Сколько Вы получаете в месяц?"))
+let slicedArr = [...part1, ...part2];
 
-const countPercent = (number) => {
-  return number / 100 * 13;
-}
-
-let totalResult = `Размер заработной платы за вычетом налогов равен ${countPercent(responce)} руб.`
-
-console.log("2 задание");
-console.log(totalResult)
+console.log(slicedArr)
 
 // 3 задание
-// Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел
+// Используя Math.random() вам необходимо генерировать цифры от 0 до 9, и создать массив состоящий из 5 таких элементов
+// 1. Рассчитать сумму элементов этого массива
+// 2. Найти минимальное число
+// 3. Найти есть ли в этом массиве число 3
 
-let numbers = [];
-
-for(let i = 0; i <= 2; i++) {
-  numbers.push(prompt("Введите число"));
-}
-
-let arrOfNumber = Array.from(numbers);
-
-const findMaxNumber = (arr) => {
-  min = arr[0];
-  max = min;
-  for (let i = 1; i < arr.length; ++i) {
-    if (arr[i] > max) max = arr[i];
-    if (arr[i] < min) min = arr[i];
+function getRndInteger() {
+  let array = [];
+  for(let i = 0; i < 5; i++) {
+    let number = Math.floor(Math.random()*10);
+    array.push(number);
   }
-  return max;
+  return array;
 }
-
-console.log("3 задание");
-console.log(findMaxNumber(arrOfNumber));
-
-// 4 задание
-// Необходимо реализовать четыре функции, каждая функция должна принимать по два числа и выполнять одну из операций (каждая функция выполняет одну из них):
-// 1. Сложение
-// 2. Разность
-// 3. Умножение
-// 4. Деление
-// Необходимо сделать так, чтобы функция вернула число, например выражение console.log(sum(2, 6)); должно вывести число 8 в консоль (sum - функция сложения в данном примере, ваши названия функций могут отличаться). Округлять значения, которые возвращают функции не нужно, однако, обратите внимание на разность, функция должна вычесть из большего числа меньшее, либо вернуть 0, если числа равны. Функциям всегда передаются корректные числа, проверки на NaN, Infinity делать не нужно.
-
-const sum = (a, b) => {
-  return a + b;
-}
-
-const subtraction = (a, b) => {
-  let arr = [a, b];
-  b = arr[0];
-  a = b;
-  for (let i = 0; i < arr.length; ++i) {
-    (arr[i] > b) ? b = arr[i] : false;
-    (arr[i] < a) ? a = arr[i]: false;
-  }
-
-  return b - a;
-}
-
-const division = (a, b) => {
-  return a / b;
-}
-
-
-const multiplication = (a, b) => {
-  return a * b;
-}
-
-console.log("4 задание");
-console.log(sum(10, 2));
-console.log(subtraction(10, 2));
-console.log(division(10, 2));
-console.log(multiplication(10, 2))
+console.log(getRndInteger())
