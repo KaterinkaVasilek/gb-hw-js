edce4d2cb321676a1f309aa73a01e91131a1acb6
KaterinkaVasilek <e.kushner@bk.ru>
2023-09-22 16:34:21 +0300

    feat last js homework

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,16 @@
 import { dataText } from "./data.js";
 
-const data = JSON.parse(dataText)
-console.log(data);
-const cardsList = document.querySelector(".cards")
+const data = JSON.parse(dataText);
+const cardsList = document.querySelector(".cards");
+const cartContainer = document.querySelector(".cart__list");
+const cart = [];
 
-data.forEach(item => {
-  let card = `
-    <li class="cards__item">
+let items = data.reduce((result, item, index) => {
+  let el = `
+    <li class="card__item" data-item="${index}">
+      <div class="card__hover">
+        <button class="card__button">Add to card</button>
+      </div>
       <img class="card__img" src=${item.img}>
       <div class="card__text-container">
         <h3 class="card__title">${item.header}</h3>
@@ -15,6 +19,40 @@ data.forEach(item => {
       </div>
     </li>
   `;
-  cardsList.innerHTML += card;
-});
+  return result + el;
+}, [])
 
+cardsList.innerHTML = items;
+
+// Обработчик клика  по кнопке
+
+cardsList.addEventListener("click", (e) => {
+  if(e.target.tagName === "BUTTON") {
+    let item = e.target.closest(".card__item");
+    let index = item.dataset.item;
+    
+    let dataItem = data[index];
+    cart.push(dataItem);
+    renderCart(cart);
+  }
+})
+
+const renderCart=(cart)=>{
+let items = cart.reduce((result, item, index) => {
+  let el = `
+    <li class="card__item" data-item="${index}">
+      <img class="card__img" src=${item.img}>
+      <div class="card__text-container">
+        <h3 class="card__title">${item.header}</h3>
+        <p class="card__price">Price: ${item.price}</p>
+        <p class="card__text">Color: ${item.color}</p>
+        <p class="card__text">Size: ${item.size}</p>
+        <p class="card__text">quantity: ${item.quantity}</p>
+      </div>
+    </li>
+  `;
+  return result + el;
+}, [])
+  
+cartContainer.innerHTML = items;
+}
